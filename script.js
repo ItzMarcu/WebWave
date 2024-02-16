fb698e2e41374af959b231f70742968903e7bacc
ItzMarcu <andreamrccc@gmail.com>
2024-02-16 16:35:27 +0100

    Front-End finished, missing Back-End

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let isPlaying = false;
+let song = '';
+
+function StartPlaying(songID) {
+    
+}
+
+let songButton = document.getElementById("play");
+songButton.addEventListener("click", function() {
+    if (isPlaying === false) {
+
+    } else {
+
+    }
+});
